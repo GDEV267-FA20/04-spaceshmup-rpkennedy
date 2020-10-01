e205be48d340d2c9ed320714193c9cdb0343f6a4
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-30 21:36:00 -0400

    SpaceShooter v0.1.1
    Some scripting done

--- Assets/__Scripts/HeroContr.cs
+++ Assets/__Scripts/HeroContr.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class HeroContr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    static public HeroContr S;
+
+    [Header("All you m8:")]
+    public float speed;
+    public float rollMult;
+    public float pitchMult;
+    public float shieldLevel;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.Log("Hero.Awake() - S already exists");
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+    void FixedUpdate()
     {
         
     }
---
+++ Assets/__Scripts/Shield.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [Header("All you m8")]
+    public float rotationsPerSecond;
+
+    private int levelShown = 0;
+    Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;    
+    }
+
+    void Update()
+    {
+        int currLevel = Mathf.FloorToInt(HeroContr.S.shieldLevel);
+
+        if (levelShown != currLevel)
+        {
+            levelShown = currLevel;
+            mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);
+        }
+
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+    }
+}
