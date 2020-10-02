4cb3cd572dbc3c35edf454259bce0717396e2307
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-10-01 20:03:21 -0400

    SpaceShooter v0.3
    Skipped 0.2 cause we flying:
    Hero Script
    BoundsCheck script
    Enemies Matted + Prefabbed

---
+++ Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("All you m8:")]
+    public float radius;
+
+    [Header("No worries m8:")]
+    public float camWidth;
+    public float camHeight;
+
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+    
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+
+        if (pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+
+        transform.position = pos;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+}
